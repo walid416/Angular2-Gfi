6f94b932401b8e2f1942c98774c907395bebf1cf
walid416 <hattabwalid2003@gmail.com>
2017-08-12 13:57:26 +0200

    edit master

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Angular2-Gfi 
+# AngularGfi
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.2.7.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './includes/header/header.component';
-import { FooterComponent } from './includes/footer/footer.component';
-import { SidebarComponent } from './includes/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule

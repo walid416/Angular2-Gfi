d3e9774d98e446de153c88d612f9bd6d6296ba75
walid416 <hattabwalid2003@gmail.com>
2017-08-16 22:41:13 +0200

    resolve probleme

--- src/app/includes/header/header.component.ts
+++ src/app/includes/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  color:string = 'red';
   constructor() { }
 
   ngOnInit() {
   }
+
+
+changeStyle($event){
+  this.color = $event.type == 'mouseover' ? 'yellow' : 'red';
+}
 
 }
